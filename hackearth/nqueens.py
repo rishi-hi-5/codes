8c4f593eaac00ec0c641eedc1d98a36b873c321e
rishi-hi-5 <rishikeshfanse@gmail.com>
2017-10-29 18:18:16 +0530

    nqueens

--- hackearth/nqueens.py
+++ hackearth/nqueens.py
@@ -2,43 +2,30 @@ n=int(input())
 num=n
 
 def check_attacking(mat,qx,qy):
-    global num
-    print("result for attacking")
-    for i in range(0,num):
-        if(qx+qy<num):
-            c1=abs(qx+qy)+1
-            c2=abs(qx+(num-1)-qy)+1
-        else:
-            c1=abs(qx-qy)+1
-            c2=abs(qx-(num-1)-qy)+1
-        print(i,qx,qy,mat[(qx+i)%num][qy]!=0,mat[qx][(qy+i)%num]!=0,mat[(qx+i)%c2][(qy+i)%c2]!=0,mat[(qx-i)%c2][(qy+i)%c2]!=0)
-        if(mat[(qx+i)%num][qy]!=0 or mat[qx][(qy+i)%num]!=0 or mat[(qx+i)%c2][(qy+i)%c2]!=0 or mat[(qx-i)%c2][(qy+i)%c2]!=0):
+    i=0
+    while(i!=len(q)):
+        qxx=q[i][0]
+        qyy=q[i][1]
+        if(qxx-qx==0 or qyy-qy==0 or abs(qxx-qx)==abs(qyy-qy)):
             return True
-    return False    
-
-def print_mat(mat):
-    global num
-    print()
-    print("new mat starts")
-    for i in range(0,num):
-        for j in range(0,num):
-            print(mat[i][j],end=' ')
-        print()
-
+        i+=1
+    return False
 
 def nqueens(n,mat):
     global num
+    global q
     if n==0:
         return True
     #1 denotes space is taken and 2 denotes queen
     for i in range(0,num):
         for j in range(0,num):
-            print_mat(mat)
             if(check_attacking(mat,i,j)==False):
-                mat[i][j]=2
+                mat[i][j]=1
+                q.append([i,j])
                 n-=1
                 if(nqueens(n,mat)):
                     return True
+                q=q[:len(q)-1]
                 n+=1
                 mat[i][j]=0
     return False
@@ -52,6 +39,7 @@ def print_me(l):
     
 
 mat=[[0 for i in range(0,n)] for i in range(0,n)]
+q=[]
 if nqueens(n,mat):
     print("YES")
     for i in range(0,n):
